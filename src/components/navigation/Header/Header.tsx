dff5d9dcb2c85a2d6f2bb90fa4a2ecb59977742b
Ajosephp <128110702+Ajosephp@users.noreply.github.com>
2024-04-09 21:30:24 -0700

    [NEW] Implemented yarn. Finished MobileMenu

--- src/components/navigation/Header/Header.tsx
+++ src/components/navigation/Header/Header.tsx
@@ -3,7 +3,6 @@
 
 import React, { useState, useEffect } from "react";
 import Link from "next/link";
-import { NavMenuIcon } from "../NavMenuIcon";
 import { MobileMenu } from "../MobileMenu/MobileMenu";
 import { DesktopMenu } from "../DesktopMenu";
 import { ModeToggle } from "@/components/ui/mode-toggle";
@@ -14,37 +13,32 @@ interface HeaderProps {
   }
   
   export const Header: React.FC<HeaderProps> = () => {
+// Set the initial width to False - we will check this below
+const [isMobileView, setIsMobileView] = useState(false);
 
-    const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
-    const [isMobileView, setIsMobileView] = useState(false);
+  useEffect(() => {
+    // Now safe to use window because this runs client-side
+    const handleResize = () => setIsMobileView(window.innerWidth < 768);
+    // Initial check when component mounts
+    handleResize();
+    // Setup event listener for subsequent resizes
+    window.addEventListener('resize', handleResize);
+    // Cleanup to remove the event listener on component unmount
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
-    useEffect(() => {
-      const checkIsMobileView = () => window.innerWidth < 768;
-      setIsMobileView(checkIsMobileView());
-      
-      // Listen to window resize events
-      window.addEventListener('resize', () => setIsMobileView(checkIsMobileView()));
-      return () => window.removeEventListener('resize', () => setIsMobileView(checkIsMobileView()));
-    }, []);
-
-  return (
-    <header className="h-16 flex items-center justify-between px-4 mb-8 border-b lg:px-8 lg:mb-12">
-
-      <div className="flex items-center flex-1">
+return (
+  <header className="h-16 flex items-center justify-between px-4 mb-8 border-b lg:px-8 lg:mb-12">
+    <div className="flex items-center flex-1">
       <Link className="text-xl font-semibold" href="/">
         DnD Tools
       </Link>
-      </div>
+    </div>
 
-      <NavMenuIcon isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
-      
-      {/* Conditionally render based on viewport size */}
-      {isMobileView ? 
-        (isMenuOpen && <MobileMenu isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />) :
-          <DesktopMenu />}
-      
-      <ModeToggle />
-    </header>
-  )
+    {/* Conditionally render based on viewport size */}
+    {isMobileView ? <MobileMenu /> : <DesktopMenu />}
+    
+    <ModeToggle />
+  </header>
+);
 };
-
--- src/components/navigation/MobileMenu/MobileMenu.tsx
+++ src/components/navigation/MobileMenu/MobileMenu.tsx
@@ -1,36 +1,63 @@
 // MobileMenu Component
 import styles from './MobileMenu.module.css';
-import { NavItem } from "../NavItem";
+
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
+import Link from 'next/link';
 
 interface MobileMenuProps {
-    isMenuOpen: boolean;
-    setIsMenuOpen: React.Dispatch<React.SetStateAction<boolean>>;
     className?: string;
 }
 
-export const MobileMenu: React.FC<MobileMenuProps> = ({ isMenuOpen, setIsMenuOpen, className }) => {
-    if (!isMenuOpen) return null;
+export const MobileMenu: React.FC<MobileMenuProps> = ({ className }) => {
 
-    return (
-        <>
-            <div className="fixed inset-0 z-10" onClick={() => setIsMenuOpen(false)}></div>
-            {/* Tailwind's dark mode prefix will handle the theme switching */}
-            <nav className={styles.nav}>
-            <ul className="text-center text-md font-medium">
-                <NavItem href="/" 
-                         onClick={() => setIsMenuOpen(false)}
-                         className="mb-1 mt-1 pb-1 pt-1">Home
-                </NavItem>
-                <NavItem href="/character-generator"
-                         onClick={() => setIsMenuOpen(false)}
-                         className="mb-1 mt-1 pb-1 pt-1">Character Generator
-                </NavItem>
-                <NavItem href="/initiative-tracker"
-                         onClick={() => setIsMenuOpen(false)}
-                         className="mb-1 mt-1 pb-1 pt-1">Initiative Tracker
-                </NavItem>
-            </ul>
-        </nav>
-        </>
+return (
+    <DropdownMenu >
+            <DropdownMenuTrigger asChild>
+                <button className="mr-4">Menu</button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className={styles.dropdownMenu}>
+                <DropdownMenuLabel>Navigation</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                {/* Wrap the DropdownMenuItem content with Link */}
+                <DropdownMenuItem asChild>
+                    <Link href="/" passHref>
+                        Home
+                    </Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem asChild>
+                    <Link href="/character-generator" passHref>
+                        Character Generator
+                    </Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem asChild>
+                    <Link href="/initiative-tracker" passHref>
+                        Initiative Tracker
+                    </Link>
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+    </DropdownMenu>
     );
 };
+//     return (
+//         <>
+//             <div className="fixed inset-0 z-10" onClick={() => setIsMenuOpen(false)}></div>
+//             {/* Tailwind's dark mode prefix will handle the theme switching */}
+//             <nav className={styles.nav}>
+//             <ul className="text-center text-md font-medium">
+//                 <NavItem href="/" 
+//                          onClick={() => setIsMenuOpen(false)}
+//                          className="mb-1 mt-1 pb-1 pt-1">Home
+//                 </NavItem>
+//                 <NavItem href="/character-generator"
+//                          onClick={() => setIsMenuOpen(false)}
+//                          className="mb-1 mt-1 pb-1 pt-1">Character Generator
+//                 </NavItem>
+//                 <NavItem href="/initiative-tracker"
+//                          onClick={() => setIsMenuOpen(false)}
+//                          className="mb-1 mt-1 pb-1 pt-1">Initiative Tracker
+//                 </NavItem>
+//             </ul>
+//         </nav>
+//         </>
+//     );
+// };
